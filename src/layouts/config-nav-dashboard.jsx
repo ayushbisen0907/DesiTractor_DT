43be2f4389ee726276e4b7319d62e5f84b334c66
Ayush Bisen <ayushbisen1309@gmail.com>
2025-05-22 01:37:36 +0530

    fghj

--- src/layouts/config-nav-dashboard.jsx
+++ src/layouts/config-nav-dashboard.jsx
@@ -9,32 +9,14 @@ import { SvgColor } from 'src/components/svg-color';
 const icon = (name) => <SvgColor src={`${CONFIG.site.basePath}/assets/icons/navbar/${name}.svg`} />;
 
 const ICONS = {
-  job: icon('ic-job'),
-  blog: icon('ic-blog'),
-  chat: icon('ic-chat'),
-  mail: icon('ic-mail'),
-  user: icon('ic-user'),
-  file: icon('ic-file'),
-  lock: icon('ic-lock'),
-  tour: icon('ic-tour'),
-  order: icon('ic-order'),
-  label: icon('ic-label'),
-  blank: icon('ic-blank'),
-  kanban: icon('ic-kanban'),
-  folder: icon('ic-folder'),
-  course: icon('ic-course'),
-  banking: icon('ic-banking'),
-  booking: icon('ic-booking'),
-  invoice: icon('ic-invoice'),
-  product: icon('ic-product'),
-  calendar: icon('ic-calendar'),
-  disabled: icon('ic-disabled'),
-  external: icon('ic-external'),
-  menuItem: icon('ic-menu-item'),
-  ecommerce: icon('ic-ecommerce'),
-  analytics: icon('ic-analytics'),
-  dashboard: icon('ic-dashboard'),
-  parameter: icon('ic-parameter'),
+  tractor: icon('ic-tractor'),
+  eqipments: icon('ic-equipments'),
+  compare: icon('ic-compare'),
+  price: icon('ic-price'),
+  showroom: icon('ic-showroom'),
+  loan: icon('ic-loans'),
+  news: icon('ic-news'),
+  more: icon('ic-more'),
 };
 
 // ----------------------------------------------------------------------
@@ -46,21 +28,24 @@ export const navData = [
   {
     subheader: 'Overview 6.0.0',
     items: [
-      { title: 'One', path: paths.dashboard.root, icon: ICONS.dashboard },
-      { title: 'Two', path: paths.dashboard.two, icon: ICONS.ecommerce },
-      { title: 'Three', path: paths.dashboard.three, icon: ICONS.analytics },
+      { title: 'Tractor', path: paths.dashboard.root, icon: ICONS.tractor },
+      { title: 'Equipments', path: paths.dashboard.two, icon: ICONS.eqipments },
+      { title: 'Compare', path: paths.dashboard.three, icon: ICONS.compare },
+      { title: 'Price', path: paths.dashboard.three, icon: ICONS.price },
+      { title: 'Showroom', path: paths.dashboard.three, icon: ICONS.showroom },
+      { title: 'Loans', path: paths.dashboard.three, icon: ICONS.loan },
+      { title: 'News & Update', path: paths.dashboard.three, icon: ICONS.news },
     ],
   },
   /**
    * Management
    */
   {
-    subheader: 'Management',
     items: [
       {
-        title: 'Group',
+        title: 'More',
         path: paths.dashboard.group.root,
-        icon: ICONS.user,
+        icon: ICONS.more,
         children: [
           { title: 'Four', path: paths.dashboard.group.root },
           { title: 'Five', path: paths.dashboard.group.five },
